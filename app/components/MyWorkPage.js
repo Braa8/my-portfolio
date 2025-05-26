0165ffd96223a37ec80647853fa7fcd3f82b383f
Braa Alshoumary <braaalshoumary5@gmail.com>
2025-05-26 10:01:46 +0300

    new fix

---
+++ app/components/MyWorkPage.js
@@ -0,0 +1,40 @@
+import Card from "../components/Card";
+import Navbar from "../components/Navbar";
+
+export default function Work () {
+    return (
+        <>
+            <Navbar />
+
+            <div className="min-h-screen py-8 px-4">
+      <div className="max-w-7xl mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 justify-items-center">
+        <Card
+          title=" Portfolio  "
+          description=" responsive portfolio with simple designes , made by next.js "
+          image="/portimage.png"
+          link="/portfolio.mp4"
+        />
+        <Card
+          title=" CSS project"
+          description=" a simple animation using HTML and TailwindCSS "
+          image="/flowerimage.png"
+          link="/flower .mp4"
+        />
+        <Card
+          title=" CRUD System"
+          description=" this system made by ( HTML , CSS , JAVA SCRIPT)"
+          image="crudimage.png"
+          link="/cruds.mp4"
+        />
+        <Card
+          title=" Scroll "
+          description=" this is a responsive project using scroll animations , made by  ( HTML , CSS , JS) "
+          image="scrollimage.png"
+          link="/scroll .mp4"
+        />
+      </div>
+    </div>
+
+        </>
+    )
+}
--- app/myWork/page.js
+++ app/myWork/page.js
@@ -1,40 +1,8 @@
-import Card from "../components/Card";
-import Navbar from "../components/Navbar";
+// pages/myWork.js
+import dynamic from 'next/dynamic';
 
-export default function Work () {
-    return (
-        <>
-            <Navbar />
+const Work = dynamic(() => import('../components/MyWorkPage'), {
+  ssr: false
+});
 
-            <div className="min-h-screen py-8 px-4">
-      <div className="max-w-7xl mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 justify-items-center">
-        <Card
-          title=" Portfolio  "
-          description=" responsive portfolio with simple designes , made by next.js "
-          image="/portimage.png"
-          link="/portfolio.mp4"
-        />
-        <Card
-          title=" CSS project"
-          description=" a simple animation using HTML and TailwindCSS "
-          image="/flowerimage.png"
-          link="/flower .mp4"
-        />
-        <Card
-          title=" CRUD System"
-          description=" this system made by ( HTML , CSS , JAVA SCRIPT)"
-          image="crudimage.png"
-          link="/cruds.mp4"
-        />
-        <Card
-          title=" Scroll "
-          description=" this is a responsive project using scroll animations , made by  ( HTML , CSS , JS) "
-          image="scrollimage.png"
-          link="/scroll .mp4"
-        />
-      </div>
-    </div>
-
-        </>
-    )
-}
+export default Work;
